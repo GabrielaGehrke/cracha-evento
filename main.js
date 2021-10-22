2b22dc80059c072cedb2b26e7cb74aca0f37bf8a
Gabriela Gehrke <66082757+GabrielaGehrke@users.noreply.github.com>
2021-10-22 12:21:32 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinkSocialMedia = {
+  github: 'gabrielagehrke',
+  youtube: '',
+  facebook: 'gabigehrke',
+  instagram: 'gabrielasgehrke',
+  twitter: ''
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].ref = `https://${social}.com/${LinkSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `http://api.github.com/users/${LinkSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+
+  //Arrow Function: .them (argumento ou (argumento, argumento2) ou (vazio)=> {...})
+}
+getGitHubProfileInfos()
